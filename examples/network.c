b3b9f67f473909c5cd6b2c446d67f358952e9d37
Hu Zheng <freetiger18@gmail.com>
2016-07-30 11:49:57 +0800

    Add an example with network (unimplemented).

---
+++ examples/network.c
@@ -0,0 +1,17 @@
+#include "acllib.h"
+#include <stdio.h>
+
+int Setup() {
+  initWindow("Test", DEFAULT, DEFAULT, 800, 600);
+  msgBox("This", "That", 0);
+  initConsole();
+  printf("Hello\n");
+
+  beginPaint();
+
+  line(10, 10, 100, 100);
+
+  endPaint();
+
+  return 0;
+}
